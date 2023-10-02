e42652a8a9bf5ed5f26a11c7e4ab175df9f33084
Константин <146705869+yasn0clown@users.noreply.github.com>
2023-10-02 10:51:20 +0300

    Add files via upload

---
+++ main1.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+app = FastAPI()
+
+@app.post("/Calculator")
+def calculator(num1:int,num2:int,operand:str):
+    if operand == "+":
+        return num1+num2
+    elif operand == "-":
+        return num1-num2
+    elif operand == "*":
+        return num1*num2
+    elif operand == "/":
+        if num2 == 0:
+            return {"На ноль делить нельзя"}
+        else:
+            return num1/num2
+    else:
+        return {"Фича всё ещё в разработке"}
